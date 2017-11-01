c060c825397302ce903b507d7a0543effc6dedfa
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-01 09:17:30 -0600

    it 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
 class Activity
+
+  attr_reader :name, :participants
+
+  def initialize(name)
+    @name = name
+    @participants = Hash.new
+  end
+
+  def add_participant(participant)
+    participant.each do |key, value|
+      @participants[key] = value
+    end
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,27 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_exists
+    activity = Activity.new('Hike')
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_attributes
+    activity = Activity.new('Hike')
+
+    assert_equal 'Hike', activity.name
+    assert_equal Hash.new, activity.participants
+  end
+
+  def test_can_add_participant_to_activity
+    activity = Activity.new('Hike')
+    activity.add_participant(name: 'Timo', amount: 5)
+
+    assert_equal 'Hike', activity.name
+    assert_equal 'Timo', activity.participants[:name]
+    assert_equal 5, activity.participants[:amount]
+  end
+
 end
